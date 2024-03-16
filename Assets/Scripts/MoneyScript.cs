b294073fefcaffe4d34d4fb0cc9776a0f812cbdb
Wookuzi <sabeder.luka30@gmail.com>
2024-03-16 23:17:33 +0100

    Novi joystick in camera

---
+++ Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyScript : MonoBehaviour
+{
+   /* public float mCount;
+    public Text mText;
+
+    void Start()
+    {
+        mCount = PlayerPrefs.GetFloat("ammount");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            mCount += 1;
+            PlayerPrefs.SetFloat("ammount", mCount);
+        }
+
+        mText.text = "Pare: " + mCount;
+    } */
+}
